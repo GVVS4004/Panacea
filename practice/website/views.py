b2cd06b47837a08ce38e163f19c2857b74b0060f
GVVS4004 <gvijay4004@gmail.com>
2022-12-05 19:39:49 +0530

    Third version commit

--- practice/website/views.py
+++ practice/website/views.py
@@ -2,90 +2,28 @@ import json
 from flask import Blueprint,jsonify
 from flask import render_template,url_for,request,redirect,session
 from flask_login import login_required,current_user
-from flask_wtf import FlaskForm
-from wtforms import FileField,SubmitField
-import pprint
+import boto3
+import sys,os
+import re
+import json
+from collections import defaultdict
+import requests,os,pprint
+import spoonacular as sp
+api = sp.API("b8cf3af47090431c8648f42c61944428")
 views =Blueprint('views',__name__)
 import json
 from . import db
 
-# # @views.route('/home')
-# # @login_required
-# def home():
-#     uid=current_user._id
-#     lis=list()
-#     len1=list()
-#     mnts=list()
-#     lis1=db.db.nutrition_info.find_one({'uid':uid})
-    
-#     if(lis1==None):
-#         lis=['None']
-#         len1=['None']
-#     else:
-#         months=lis1['months']
-#         print(lis1)
-#         i=0
-#         j=0
-#         for month in months:
-#             templis=list()
-            
-#             mnts.append(month)
-#             for value in months[month].values():
-#                 templis.append(value)
-#             lis.append(templis)
-        
-#         for i in range(len(lis)):
-#             len1.append(i)
-#     print(lis)
-#     print(mnts)
-#     jan=[]
-#     if lis1!=None:
-#         for i in lis1['months']['January']:
-#             jan.append(lis1['months']['January'][str(i)])
-#         feb=[]
-#         for i in lis1['months']['February']:
-#             feb.append(lis1['months']['February'][str(i)])
-#         mar=[]
-#         for i in lis1['months']['March']:
-#             mar.append(lis1['months']['March'][str(i)])
-#         apr=[]
-#         for i in lis1['months']['April']:
-#             apr.append(lis1['months']['April'][str(i)])
-#         may=[]
-#         for i in lis1['months']['May']:
-#             may.append(lis1['months']['May'][str(i)])
-#         jun=[]
-#         for i in lis1['months']['June']:
-#             jun.append(lis1['months']['June'][str(i)])
-#         jul=[]
-#         for i in lis1['months']['July']:
-#             jul.append(lis1['months']['July'][str(i)])
-#         aug=[]
-#         for i in lis1['months']['August']:
-#             aug.append(lis1['months']['August'][str(i)])
-#         sep=[]
-#         for i in lis1['months']['September']:
-#             sep.append(lis1['months']['September'][str(i)])
-#         oct=[]
-#         for i in lis1['months']['October']:
-#             oct.append(lis1['months']['October'][str(i)])
-#         nov=[]
-#         for i in lis1['months']['November']:
-#             nov.append(lis1['months']['November'][str(i)])
-#         dec=[]
-#         for i in lis1['months']['December']:
-#             dec.append(lis1['months']['December'][str(i)])
 
-    
-
-#     # print('jan:',jan)
-#     return render_template('odash.html',user=current_user,jan=jan,feb=feb,mar=mar,apr=apr,may=may,jun=jun,jul=jul,aug=aug,sep=sep,oct=oct,nov=nov,dec=dec)
+#HOME
 @views.route('/')
 def home():
     return render_template('home.html')
 @views.route('/items',methods=['POST','GET'])
 def items():
     return render_template("index.html")
+
+# USER DASH_BOARD
 @views.route('/pie')
 @login_required
 def pie():
@@ -95,27 +33,6 @@ def pie():
     mnts=list()
     years=list()
     lis1=db.db.nutrition_info.find_one({'uid':uid})
-    
-    # if(lis1==None):
-        # lis=['None']
-        # len1=['None']
-
-    # else:
-    #     years=lis1['years']
-    #     for year in years:
-    #         months=lis1['years'][year]['months']
-    #         i=0
-    #         j=0
-    #         for month in months:
-    #             templis=list()
-    #             mnts.append(month)
-    #             for value in months[month].values():
-    #                 templis.append(value)
-    #             lis.append(templis)
-            
-    #         for i in range(len(lis)):
-    #             len1.append(i)
-    # res=[0]*len(mnts)
     firstmonth=None
     firstyear=None
     if lis1!=None:
@@ -131,11 +48,12 @@ def pie():
                 firstmonth=month
         print(lis)
     bl=False
-    
-
     if len(lis)!=0:
         bl=True
     return render_template('dashboard.html',user=current_user,res=lis,bl=bl,firstmonth=firstmonth,firstyear=firstyear)
+
+
+
 @views.route("/base")
 def base():
     return render_template('dash_base.html')
@@ -195,13 +113,13 @@ def base():
 #                 temp['food_id']=None
 #             items.append(temp)
 #     return render_template('item.html',items=items,name=name)
+
+#---------------------------------------------------------------FOOD INFO ROUTE------------------------------------------------------
+#MAKE CHANGES HERE
+
 @views.route('/<name>/info')
 def info(name):
-    import requests,os,pprint
-    import spoonacular as sp
-    
-    url = 'https://api.spoonacular.com/food/products/search?apiKey=&query={name}'
-    api = sp.API("")
+    # url = 'https://api.spoonacular.com/food/products/search?apiKey=b8cf3af47090431c8648f42c61944428&query={name}'
     response = api.parse_ingredients(str(name))
     data = response.json()
     quant=data[0]['amount']
@@ -209,7 +127,7 @@ def info(name):
     nutrition=data[0]['nutrition']['nutrients']
     name1=data[0]['name']
     spoonacular_api_url=requests.get(url)
-    convert_url='https://api.spoonacular.com/recipes/convert?apiKey=&ingredientName=uraddal&sourceAmount='+str(quant)+'&sourceUnit='+srcunit+'&targetUnit=grams'
+    convert_url='https://api.spoonacular.com/recipes/convert?apiKey=b8cf3af47090431c8648f42c61944428&ingredientName=uraddal&sourceAmount='+str(quant)+'&sourceUnit='+srcunit+'&targetUnit=grams'
     spoonacular_api_convert_url=requests.get(convert_url)
     amt=spoonacular_api_convert_url.json()
     amount=amt['targetAmount']
@@ -219,7 +137,9 @@ def info(name):
     print(name1)
     print(amount)
     pprint.pprint(nutrition)
-    return render_template('cart.html')
+    return render_template('')
+
+#----------------------------------------------------ADDING ITEMS TO THE USERS CART--------------------------------------------------------
 @views.route("/addcart/<food_id>",methods=['POST'])
 def addcart(food_id):
     food_name=request.form.get('foodname'+str(food_id))
@@ -228,8 +148,6 @@ def addcart(food_id):
         food_name=request.form.get('foodname'+str(food_id))
         food_info={request.form.get('foodinfo'+str(food_id))}
         print('foodname'+str(food_id))
-        # print(food_name)
-        # print(food_info)
         print(((food_info)))
         url='/item/'+str(food_name)
         # print(url)
@@ -237,7 +155,7 @@ def addcart(food_id):
         if db.db.carts.find_one({'uid':uid}):
             # print(db.db.carts.find_one({'uid':uid}))
             import spoonacular as sp
-            api = sp.API("")
+            api = sp.API("b8cf3af47090431c8648f42c61944428")
             temp_response=api.parse_ingredients(food_name)
             data = temp_response.json()
             src='https://spoonacular.com/cdn/ingredients_100x100/'+str(data[0]['image'])
@@ -254,7 +172,7 @@ def addcart(food_id):
             # db.db.carts.insert_one({'uid':uid,'food_items':{'food_id':food_id,'food_name':food_name,'quant':food_wt}})
         else:
             import spoonacular as sp
-            api = sp.API("")
+            api = sp.API("b8cf3af47090431c8648f42c61944428")
             temp_response=api.parse_ingredients(food_name)
             data = temp_response.json()
             src='https://spoonacular.com/cdn/ingredients_100x100/'+str(data[0]['image'])
@@ -334,14 +252,12 @@ def addcart(food_id):
 
 #     return render_template('cart.html',items=items)
 
+#------------------------------------------------------SEARCHING A PARTICULAR ITEM AND IT'S NUTRITIONAL VALUES-------------------------------------
 
 @views.route('/item/<name>')
 def item(name):
-    import requests,os,pprint
-    import spoonacular as sp
-    api = sp.API("")
-    autocomplete_url='https://api.spoonacular.com/food/ingredients/autocomplete?apiKey=&query={name}'
-    autocomplete_results=requests.get(autocomplete_url)
+    # autocomplete_url='https://api.spoonacular.com/food/ingredients/autocomplete?apiKey=b8cf3af47090431c8648f42c61944428&query={name}'
+    # autocomplete_results=requests.get(autocomplete_url)
     autocomplete_result=api.autocomplete_ingredient_search(name,number=5)
     autocomplete_results=autocomplete_result.json()
     pprint.pprint(autocomplete_results)
@@ -360,7 +276,6 @@ def item(name):
             # temp['food_id']=autocomplete_results['id']
             temp_response=api.parse_ingredients(autocomplete_name)
             data = temp_response.json()
-            # pprint.pprint(data)
             quant=data[0]['amount']
             srcunit=data[0]['unit']
             temp['food_id']=data[0]['id']
@@ -388,38 +303,59 @@ def item(name):
         return render_template('item.html',error=True)
     return render_template('item.html',items=autocomplete_items,error=False)
 
+#---------------------------------------DISPLAYING THE ITEMS IN USER'S CART-----------------------------------------
+
 @views.route('/cart',methods=['POST','GET'])
-def cart():
+def cart(upload=False):
     uid=current_user._id
-    cur_user_items=db.db.carts.find_one({'uid':uid})['food_items']
-    # print(cur_user_items)
+    empty=False
     items=[]
-    for j in cur_user_items:
-        temp={}
-        cur_food_id=j
-        cur_food_name=cur_user_items[str(j)]['food_name']
-        cur_food_wt=cur_user_items[str(j)]['quant']
-        cur_food_src=cur_user_items[str(j)]['src']
-        cnvrt_amt=cur_user_items[str(j)]['convert_amt']
-        nutrients=(cur_user_items[str(j)]['nutrition']['nutrients'])
-        print(j)
-        for nutrient in nutrients:
-            if nutrient['name']=='Fat':
-                temp['fats']=round(((nutrient['amount'])*float(cur_food_wt))/float(cnvrt_amt),3)
-            elif nutrient['name']=='Protein':
-                temp['pro']=round(((nutrient['amount'])*float(cur_food_wt))/float(cnvrt_amt),3)
-            elif nutrient['name']=='Carbohydrates':
-                temp['carbs']=round(((nutrient['amount'])*float(cur_food_wt))/float(cnvrt_amt),3)
-            elif nutrient['name']=='Calories':
-                temp['cal']=round(((nutrient['amount'])*float(cur_food_wt))/float(cnvrt_amt),3)
-            elif nutrient['name']=='Sugar':
-                temp['sug']=round(((nutrient['amount'])*float(cur_food_wt))/float(cnvrt_amt),3)
-        temp['food_id']=cur_food_id
-        temp['food_name']=cur_food_name
-        temp['food_wt']=cur_food_wt
-        temp['src']=cur_food_src
-        items.append(temp)
-    return render_template('cart.html',items=items)
+    
+
+    if db.db.carts.find_one({'uid':uid}):
+        cur_user_items=db.db.carts.find_one({'uid':uid})['food_items']
+        # print(cur_user_items)
+        
+        for j in cur_user_items:
+            temp={}
+            cur_food_id=j
+            cur_food_name=cur_user_items[str(j)]['food_name']
+            cur_food_wt=cur_user_items[str(j)]['quant']
+            cur_food_src=cur_user_items[str(j)]['src']
+            cnvrt_amt=cur_user_items[str(j)]['convert_amt']
+            nutrients=(cur_user_items[str(j)]['nutrition']['nutrients'])
+            print(j)
+            for nutrient in nutrients:
+                if nutrient['name']=='Fat':
+                    temp['fats']=round(((nutrient['amount'])*float(cur_food_wt))/float(cnvrt_amt),3)
+                elif nutrient['name']=='Protein':
+                    temp['pro']=round(((nutrient['amount'])*float(cur_food_wt))/float(cnvrt_amt),3)
+                elif nutrient['name']=='Carbohydrates':
+                    temp['carbs']=round(((nutrient['amount'])*float(cur_food_wt))/float(cnvrt_amt),3)
+                elif nutrient['name']=='Calories':
+                    temp['cal']=round(((nutrient['amount'])*float(cur_food_wt))/float(cnvrt_amt),3)
+                elif nutrient['name']=='Sugar':
+                    temp['sug']=round(((nutrient['amount'])*float(cur_food_wt))/float(cnvrt_amt),3)
+            temp['food_id']=cur_food_id
+            temp['food_name']=cur_food_name
+            temp['food_wt']=cur_food_wt
+            temp['src']=cur_food_src
+            items.append(temp)
+            print(len(items))
+            if len(items)==0:
+                empty=True
+    if len(items)==0:
+        empty=True
+    msg=False
+    
+    upload=request.args.get(upload)
+    if upload==True:
+        msg=True
+    print(upload)
+    return render_template('cart.html',items=items,empty=empty,msg=msg)
+
+#------------------------------------------------DELETING THE ITEM'S IN THE CART------------------------------------------------
+
 @views.route('/delete/<food_id>',methods=['POST'])
 def item_delete(food_id):
     
@@ -482,43 +418,26 @@ def analysis():
                 years[cur_year]=tempdict
                 db.db.nutrition_info.update_one({'uid':uid},{"$set":{'years':years}})
         else:
-            # cur_month=cur_month_nutri_info
             db.db.nutrition_info.insert_one({'uid':uid,"years":{cur_year:{(cur_month):cur_month_nutri_info}}})
+        db.db.carts.find_one_and_delete({'uid':uid})
     return redirect(url_for('views.pie'))
 
+#------------------------------------------CREATING A AWS TEXTRACT CLASS-----------------------------------------------------
 
-class UploadFileForm(FlaskForm):
-    file=FileField('File')
-    submit=SubmitField('Upload')
-
-@views.route('/upload',methods=['POST','GET'])
-def uploadedfile():
-    if request.method=='POST':
-        import boto3
-        import sys,os
-        import re
-        import json
-        from collections import defaultdict
-
-
+class texTract:
         def get_kv_map(file_name):
             with open(os.path.join(os.path.dirname(__file__),'static/image_uploads/'+file_name), 'rb') as file:
                 img_test = file.read()
                 bytes_test = bytearray(img_test)
                 print('Image loaded', file_name)
 
-            # process using image bytes
             client = boto3.client('textract', region_name="", aws_access_key_id='',aws_secret_access_key= '')
             response = client.analyze_document(Document={'Bytes': bytes_test,
             'S3Object':{
                 'Bucket':"penaceatextract",
-                # 'Name':key_name,
             }}, FeatureTypes=['FORMS'])
 
-            # Get the text blocks
             blocks = response['Blocks']
-
-            # get key and value maps
             key_map = {}
             value_map = {}
             block_map = {}
@@ -537,9 +456,9 @@ def uploadedfile():
         def get_kv_relationship(key_map, value_map, block_map):
             kvs = defaultdict(list)
             for block_id, key_block in key_map.items():
-                value_block = find_value_block(key_block, value_map)
-                key = get_text(key_block, block_map)
-                val = get_text(value_block, block_map)
+                value_block = texTract.find_value_block(key_block, value_map)
+                key = texTract.get_text(key_block, block_map)
+                val = texTract.get_text(value_block, block_map)
                 kvs[key].append(val)
             return kvs
 
@@ -580,21 +499,18 @@ def uploadedfile():
 
 
         def main(file_name):
-            key_map, value_map, block_map = get_kv_map(file_name)
+            key_map, value_map, block_map = texTract.get_kv_map(file_name)
 
             # Get Key Value relationship
-            kvs = get_kv_relationship(key_map, value_map, block_map)
+            kvs = texTract.get_kv_relationship(key_map, value_map, block_map)
             print("\n\n== FOUND KEY : VALUE pairs ===\n")
-            print_kvs(kvs)
+            texTract.print_kvs(kvs)
 
 
             # Start searching a key value
             # while input('\n Do you want to search a value for a key? (enter "n" for exit) ') != 'n':
             #     search_key = input('\n Enter a search key:')
             #     print('The value is:', search_value(kvs, search_key))
-            import requests,os,pprint
-            import spoonacular as sp
-            api = sp.API("")
             uid=current_user._id
             for key,value in kvs.items(): 
                 if db.db.carts.find_one({'uid':uid}):
@@ -630,10 +546,21 @@ def uploadedfile():
                         db.db.carts.insert_one({'uid':uid,'food_items':{str(food_id):{'_id':str(food_id),'food_name':key,'quant':int(value[0]),'convert_amt':cnvrt_amt,'src':src,'nutrition':nutrition}}})
                     except:
                         pass
+            print('main end')
+
+#----------------------------------------------------------UPLOADING THE ITEMS FROM A DOCUMENT---------------------------------------------
+
+@views.route('/upload',methods=['POST','GET'])
+def upload():
+    if request.method=='POST':
+        
 
         file_name=request.files.get('file')
         file_name.save(os.path.join(os.path.dirname(__file__),'static/image_uploads/'+file_name.filename))
-        print(file_name)
-        main(file_name.filename)
-        return redirect(url_for('views.cart'))
-    return render_template('uploadfile.html')
+        try :
+            texTract.main(file_name.filename)
+            return redirect(url_for('views.cart',upload=True))
+        except:
+            return render_template('uploadfile.html',err=True)
+
+    return render_template('uploadfile.html',err=False)
